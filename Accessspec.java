ad697c0fcdbfa7f4a5f7e67c310710a95bf2c2ad
MohamedYaheeya <109885949+MohamedYaheeya@users.noreply.github.com>
2022-09-28 18:23:59 +0530

    Add files via upload

---
+++ Accessspec.java
@@ -0,0 +1,17 @@
+class Accessspec{
+
+        String name;
+        int age;
+        String address;
+        String email;
+        private String password;
+
+void Detail(){
+
+        System.out.println("Name : "+ name );
+        System.out.println("Age : "+ age );
+        System.out.println("Address : "+ address );
+        System.out.println("Email : "+ email );
+
+}
+}
---
+++ Collection.java
@@ -0,0 +1,11 @@
+class   Collection{
+    public static void main(String[] args){
+            Accessspec show = new Accessspec();
+            show.name = "Wahidha";
+            show.age = 19;
+            show.address = "Perambalur";
+            show.email = "myaheeya@gmail.com";
+            show.Detail();
+
+    }
+}
