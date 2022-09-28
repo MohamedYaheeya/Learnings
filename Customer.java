342fcee032381a4cc6e23719bf9f0574afad2f8f
MohamedYaheeya <109885949+MohamedYaheeya@users.noreply.github.com>
2022-09-28 21:05:16 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,9 @@
+class   Customer{ 
+
+     public static void main(String[] args){
+      Painter Hari = new Painter();
+      String result = Hari.Drawpainting();
+      System.out.println("Return type Result "+ result);
+
+ }
+}
---
+++ Painter.java
@@ -0,0 +1,9 @@
+class Painter{ 
+ 
+ String Drawpainting(){ 
+ System.out.println("Im a Painter");
+ return "painting";
+
+ }
+
+}
