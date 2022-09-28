887a6bbf2cd425d8047435fb8593858dd172e025
MohamedYaheeya <109885949+MohamedYaheeya@users.noreply.github.com>
2022-09-28 17:34:54 +0530

    Add files via upload

---
+++ Laptop.java
@@ -0,0 +1,21 @@
+class Laptop {
+ 
+    String modelName ;
+
+    void Detail() {
+
+          modelName = "Lenovo Ideal pad";
+          System.out.println("ModelName :" + modelName);
+
+    }
+
+    public static void main(String[] args) {
+     
+     
+          Laptop show = new Laptop();
+          show.Detail();
+          
+         
+    }
+
+}
